3cccecd4b2fbcb6a2cddcd7e39f3020af63ac9e2
Leandro Strelow <kikostrelow@gmail.com>
2026-05-24 12:00:18 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,35 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'ilha-play-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './logo.png',
+  './bannerviafor.jpg',
+  './cincate.png',
+  './camisapreta.png',
+  './camisaroxa.png',
+  './camisaverde.png',
+  './bolas.png',
+  './cordas.png',
+  './ford.png',
+  './disk.png',
+  './plus.png',
+  './otica.png',
+  './lotus.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
